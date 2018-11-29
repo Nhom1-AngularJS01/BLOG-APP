ee266bb7cb1780ba0399bf9af291bfb97b461c57
Dung <kiduyoona@gmail.com>
2018-11-29 21:48:36 +0700

    update file App

--- Home/component.js
+++ Home/component.js
@@ -1,7 +1,7 @@
 (() => {
     angular.module("BlogApp")
         .component('home', {
-            templateUrl: 'templates/home.html',
+            templateUrl: 'Home/home.html',
             controller: 'homeController'
         })
 })();
--- js/ui-router/router.js
+++ js/ui-router/router.js
@@ -8,43 +8,43 @@
         {
             name: 'article',
             url: '/article/:slug',
-            templateUrl: 'templates/articlePage.html',
+            templateUrl: 'Article/articlePage.html',
             controller: 'articleController'
         },
         {
             name: `signIn`,
             url: `login`,
-            templateUrl: `templates/login.html`,
+            templateUrl: `SignIn-SignUp/login.html`,
             controller: "UserSignIn"
         },
         {
             name: `signUp`,
             url: `register`,
-            templateUrl: `templates/register.html`,
+            templateUrl: `SignIn-SignUp/register.html`,
             controller: "UserSignUp"
         },
         {
             name: `settings`,
             url: `settings`,
-            templateUrl: `templates/settings.html`,
+            templateUrl: `Setting/settings.html`,
             controller: `Settings`
         },
         {
             name: "profileUser",
             url: "/@:username",
-            templateUrl: "templates/profileUser.html",
+            templateUrl: "Profile/profileUser.html",
             controller: "ProfileUser"
         },
         {
             name: "profileUser.myAticle",
             url: "/favorites",
-            templateUrl: "templates/profileAticles.html",
+            templateUrl: "Profile/profileAticles.html",
             controller: "FavoritedAticles"
         },
         {
             name: `newArticle`,
             url: `editor/:slug`,
-            templateUrl: `templates/create-editArticle.html`,
+            templateUrl: `Create-EditArticle/create-editArticle.html`,
             controller: `editor`
         }];
         state.forEach(state => {
